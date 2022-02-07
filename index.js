9f99a95b71c27c7a55180383222c6ee2acdd5715
bsamu <bsamuel0220@gmail.com>
2022-02-07 10:01:35 +0100

    2

--- index.js
+++ index.js
@@ -7,13 +7,14 @@ const add = (x, y) => {
     return Math.floor(x + y);
 };
 
-const result = add(2, 3);
-fileSystem.writeFileSync("./myFile.txt", result.toString());
+// fileSystem.writeFileSync("./myFile.txt", result.toString()); //myFile.txt-t létrehoz, beleírja az eredményt
 
 //create a server object:
 http
     .createServer(function (req, res) {
+        const result = add(2, 3);
         res.write(result.toString()); //write a response to the client
         res.end(); //end the response
     })
     .listen(8080); //the server object listens on port 8080
+// http://localhost:8080/?num1=2&num2=5 ezt akarjuk elküldeni (query), hogyan? 
