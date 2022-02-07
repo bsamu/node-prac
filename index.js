b81d9b418052b8804800f7cd3d30deee7f30f7f6
bsamu <bsamuel0220@gmail.com>
2022-02-07 10:06:53 +0100

    3

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fileSystem = require("fs");
 const http = require("http");
+const url = require("url");
 
 const add = (x, y) => {
     if ((x + y) > 10) return "sok";
@@ -12,9 +13,12 @@ const add = (x, y) => {
 //create a server object:
 http
     .createServer(function (req, res) {
-        const result = add(2, 3);
+        const queryObject = url.parse(req.url, true).query;
+        console.log(queryObject);
+        const result = add(queryObject.num1, queryObject.num2);
         res.write(result.toString()); //write a response to the client
         res.end(); //end the response
     })
     .listen(8080); //the server object listens on port 8080
-// http://localhost:8080/?num1=2&num2=5 ezt akarjuk elküldeni (query), hogyan? 
+// http://localhost:8080/?num1=2&num2=5 ezt akarjuk elküldeni (query), hogyan? const queryObject... sorral kezdünk
+// ha így futtatom először böngészőben fentit, akkor itt kiírja terminalba a dolgokat, ott meg hogy "sok", mert a 2 számot stringként kapja
